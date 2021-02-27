abd70de5ebe24134a6c95f4560505332e3a6a7c9
Weazzzzzzey <heronightmares@gmail.com>
2021-02-28 00:59:09 +0200

    Add project files.

---
+++ AntraUzduotisIntelektika/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace AntraUzduotisIntelektika
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ReadDataButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Names = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HeightCM = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Weight = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KMIClass = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KMI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Gender = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ReadDataButton
+            // 
+            this.ReadDataButton.Location = new System.Drawing.Point(12, 415);
+            this.ReadDataButton.Name = "ReadDataButton";
+            this.ReadDataButton.Size = new System.Drawing.Size(181, 23);
+            this.ReadDataButton.TabIndex = 1;
+            this.ReadDataButton.Text = "Pasirinkti failą";
+            this.ReadDataButton.UseVisualStyleBackColor = true;
+            this.ReadDataButton.Click += new System.EventHandler(this.ReadDataButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Failo vieta: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(86, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Names,
+            this.HeightCM,
+            this.Weight,
+            this.KMIClass,
+            this.KMI,
+            this.Gender});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(724, 375);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Names
+            // 
+            this.Names.HeaderText = "Vardas";
+            this.Names.MinimumWidth = 6;
+            this.Names.Name = "Names";
+            this.Names.Width = 125;
+            // 
+            // HeightCM
+            // 
+            this.HeightCM.HeaderText = "Ūgis";
+            this.HeightCM.MinimumWidth = 6;
+            this.HeightCM.Name = "HeightCM";
+            this.HeightCM.Width = 60;
+            // 
+            // Weight
+            // 
+            this.Weight.HeaderText = "Svoris";
+            this.Weight.MinimumWidth = 6;
+            this.Weight.Name = "Weight";
+            this.Weight.Width = 60;
+            // 
+            // KMIClass
+            // 
+            this.KMIClass.HeaderText = "Klasė";
+            this.KMIClass.MinimumWidth = 6;
+            this.KMIClass.Name = "KMIClass";
+            this.KMIClass.Width = 125;
+            // 
+            // KMI
+            // 
+            this.KMI.HeaderText = "KMI";
+            this.KMI.MinimumWidth = 6;
+            this.KMI.Name = "KMI";
+            this.KMI.Width = 60;
+            // 
+            // Gender
+            // 
+            this.Gender.HeaderText = "Lytis";
+            this.Gender.MinimumWidth = 6;
+            this.Gender.Name = "Gender";
+            this.Gender.Width = 60;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1114, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ReadDataButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ReadDataButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Names;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HeightCM;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Weight;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KMIClass;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KMI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Gender;
+    }
+}
+
---
+++ AntraUzduotisIntelektika/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AntraUzduotisIntelektika
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Hide();
+        }
+
+        private void ReadDataButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            label2.Text = openFileDialog.FileName;
+            label2.Show();
+            ShowData();
+        }
+
+
+        private void ShowData()
+        {
+
+            string[] lines = File.ReadAllLines(label2.Text);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] OneLine = lines[i].Split(';');
+
+                string[] DataLine = OneLine[0].Split(',');
+
+                string Name = DataLine[0];
+                int Height = Convert.ToInt32(DataLine[1]);
+                int Weight = Convert.ToInt32(DataLine[2]);
+                string ClassKMI = DataLine[3];
+                float KMI = Convert.ToSingle(DataLine[4].Replace('.',','));
+                int Gender = Convert.ToInt32(DataLine[5]);
+
+                dataGridView1.Rows.Add(Name, Height, Weight, ClassKMI, KMI, Gender);
+
+
+            }
+
+        }
+    }
+
+}
